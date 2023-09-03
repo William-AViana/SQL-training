d49de124ad439425f621d47be8dbc997fffa9db3
William Viana <willdev.viana@gmail.com>
2023-09-03 17:09:13 -0300

    first commit with select sql

---
+++ select.sql
@@ -0,0 +1,4 @@
+--# You are given a tavle 'makeupperercase' with column 's', return a table with column 's' and 
+-- your result in a column named 'res'.
+SELECT s AS s, UPPER(s) AS res
+FROM makeuppercase;
